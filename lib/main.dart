bccd8bc01ac4489099a3a1888addec0818c71912
pratik329 <pratikraut329@gmail.com>
2021-06-05 15:23:22 +0530

    ch2

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,24 @@ class HomaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Pre6"),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("hello"),
+        appBar: AppBar(
+          title: Text("Pre6"),
         ),
-      ),
-    );
+        body: Center(
+            child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.teal,
+              gradient: LinearGradient(
+                  colors: [Colors.orange, Colors.white, Colors.green])),
+          child: Text(
+            "Box",
+            style: TextStyle(color: Colors.blue),
+          ),
+        )));
   }
 }
